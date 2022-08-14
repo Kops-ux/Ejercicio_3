4e32b75b62d9dca68cb2c3d8ef62b24c17fd5541
Kops-ux <110034772+Kops-ux@users.noreply.github.com>
2022-08-14 16:20:03 -0500

    Ejercicio 3

---
+++ Coche.java
@@ -0,0 +1,21 @@
+public class Coche {
+
+
+    public static void main(String[] args){
+        int miCoche = 1;
+        int numPuertas = 4;
+
+        incremento(numPuertas, miCoche);
+
+
+
+    }
+
+    public static void incremento(int a, int b){
+        int resultado;
+        resultado = a + b;
+
+        System.out.print(resultado);
+
+    }
+}
---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args){
+        suma_3(20, 40, 80);
+
+
+    }
+
+    public static void suma_3(int a, int b, int c) {
+        int resultado;
+
+        resultado = a + b + c;
+
+        System.out.println(resultado);
+    }
+
+}
